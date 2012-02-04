7dc395a9e41db034f364a287e9dae3d6f0f5f4c5
Prasanna Gautam <prasannagautam@gmail.com>
2012-02-04 14:29:21 -0500

    added a website in front of it

---
+++ public/javascripts/node-lua.js
@@ -0,0 +1,18 @@
+$(document).ready(function(){
+  var editor = CodeMirror.fromTextArea(document.getElementById("luaCodeBox"), {
+    mode: "lua",
+    tabMode: "indent",
+    lineNumbers: true,
+    lineWrapping: true,
+    onCursorActivity: function() {
+      editor.setLineClass(hlLine, null);
+      hlLine = editor.setLineClass(editor.getCursor().line, "activeline");
+    }
+  });
+  var hlLine = editor.setLineClass(0, "activeline");
+  $("#runLua").click(function(){
+    $.post("/runLua", {"code": editor.getValue()}, function(data){
+      $("#output").html(data);
+    });
+  });
+});
---
+++ routes/index.js
@@ -0,0 +1,23 @@
+var node_lua = require('../build/Release/node_lua')
+/*
+ * GET home page.
+*/
+
+exports.index = function(req, res){
+  res.render('index', { title: 'Express' })
+};
+
+exports.runLua = function(req, res){
+  output = "";
+  node_lua.outputCallback(function( msg){
+    //res.send(msg);
+    //console.log(msg);
+    output += msg;
+    output +="\n";
+  });
+
+  code = req.body.code;
+  console.log(code);
+  node_lua.runLua(code);
+  res.send(output);
+};
