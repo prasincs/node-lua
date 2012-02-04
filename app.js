3f9f25581b30ab6f97a48b100bc0868983d2be3c
Prasanna Gautam <prasannagautam@gmail.com>
2012-02-04 18:52:10 -0500

    moer heroku stuff

--- app.js
+++ app.js
@@ -31,6 +31,6 @@ app.configure('production', function(){
 
 app.get('/', routes.index);
 app.post('/runLua', routes.runLua);
-
-app.listen(3000);
+var port = process.env.PORT || 3000;
+app.listen(port);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
